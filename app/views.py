33e6828675a7766715cbef7bf7e7db0dd19369f2
Riza Fahmi <rizafahmi@gmail.com>
2015-04-15 16:20:39 +0700

    RethinkDB setup

--- app/views.py
+++ app/views.py
@@ -2,6 +2,44 @@ from flask import render_template, url_for, redirect, g
 from app import app
 from forms import TaskForm
 
+# rethink imports
+import rethinkdb as r
+from rethinkdb.errors import RqlRuntimeError, RqlDriverError
+
+# rethink config
+RDB_HOST = 'localhost'
+RDB_PORT = 28015
+TODO_DB = 'todo'
+
+def dbSetup():
+    connection = r.connect(host=RDB_HOST, port=RDB_PORT)
+    try:
+        r.db_create(TODO_DB).run(connection)
+        r.db(TODO_DB).table_create('todos').run(connection)
+        print 'Database setup completed'
+    except RqlRuntimeError:
+        print 'Database already exists.'
+    finally:
+        connection.close()
+
+dbSetup()
+
+# Open connection before each request
+@app.before_request
+def before_request():
+    try:
+        g.rdb_conn = r.connect(host=RDB_HOST, port=RDB_PORT, db=TODO_DB)
+    except RqlDriverError:
+        abort(503, "Database connection could not be established.")
+
+# Close the connection after each request
+#app.teardown_request
+def teardown_request(exception):
+    try:
+        g.rdb_conn.close()
+    except AttributeError:
+        pass
+
 @app.route("/")
 def index():
     form = TaskForm()
